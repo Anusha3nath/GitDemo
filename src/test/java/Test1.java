98effbefef4b24f9c55a5b795152bfdc975e4c05
anusha3nath <anusha3nath@gmail.com>
2022-04-05 16:33:08 +0530

    Helooo Find the Following

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -3,9 +3,35 @@ public class Test1 {
 
 	public static void main(String[] args) 
 	{
-		int a=10;
-		int b=20;
-		System.out.println(a+b);
+
+		// TODO Auto-generated method stub
+
+		String s="Welcome";
+		String s1="to java";
+		
+		System.out.println(s.length()); //length of a string
+		System.out.println(s.concat(s1));//joining of string
+		
+		//s="   Welcome  ";
+		System.out.println(s.trim());
+		System.out.println(s.charAt(4));
+		System.out.println(s.contains("Wel"));
+		System.out.println(s.equals("Welcome"));
+		System.out.println(s.equalsIgnoreCase("Welcome"));
+		System.out.println(s.replace('e','a'));
+		s="Welcome to java";
+		System.out.println(s.replace("java","selenium"));
+		s="Welcome";
+		System.out.println(s.substring(0,4));
+		
+		//converting cases
+		s="Welcome";
+		System.out.println(s.toLowerCase());
+		
+		s="Welcome";
+		System.out.println(s.toUpperCase());
+		
 	}
+
 
 }
